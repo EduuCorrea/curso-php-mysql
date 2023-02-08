1c0697864e4e9107bbc2f302f7355a285c2ffe1d
eduardo.correa1 <Corredudu20>
2023-02-08 14:07:35 -0300

    ok

---
+++ aula01-02-2023/fichacadastro.php
@@ -0,0 +1,52 @@
+<?php
+
+$html = '
+    style>
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+}
+
+    body {
+
+    display: flex;
+    justify-content; center;
+    aling-items; center;
+}
+
+form {
+
+    border: 1px solid blueviolet;
+    border-radius: 15px;
+    padding:25px;
+    display: flex;
+    flex-direction: column;
+    gap:15px
+}
+
+.campo {
+
+    display: flex;
+    flex-direction: column;
+    width: 100%:
+}
+
+li {
+
+    list-style: none;
+    display: flex;
+    gap: 10px;
+}
+
+input[type="submit"]{
+    padding: 10px 15px;
+    border-radius: 15px;
+    border: 1px solid blueviolet;
+    widht: 200px;
+    transition: all .4s ease-in-out;
+}
+
+
+';
---
+++ aula31012023/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//ficha de emprego, qye sera enviada para a empresa 
+//Ao final sera salvo no sistema da empresa 
+//usando arquivo de textos 
+
+$formulario_html = '
+<form>  
+    <label>Nome:</labe>
+    input type="text"
+    <label> 
+</form>
+';
